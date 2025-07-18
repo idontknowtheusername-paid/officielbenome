6996140e165d39e54a7d49478d817e4bf54e1e2e
home11101 <ton-email-home11101@example.com>
2025-07-18 16:27:29 +0100

    Nettoyage : suppression des fichiers racine, utilisation unique de officielbenome/ comme source du projet

--- src/components/dashboard/index.js
+++
@@ -1,5 +0,0 @@
-export { default as StatsCard } from './StatsCard';
-export { default as ListingCard } from './ListingCard';
-export { default as MessageCard } from './MessageCard';
-export { default as ActivityFeed } from './ActivityFeed';
-export { default as QuickActions } from './QuickActions'; 
