bc972a05cd2b81854a072e0a6a4718635c0d78fc
home11101 <ton-email-home11101@example.com>
2025-08-17 20:10:03 +0100

    Fix: Correction de l'erreur 'cn' dans RadioGroup - Utilisation de classes CSS directes

--- src/components/ui/radio-group.jsx
+++ src/components/ui/radio-group.jsx
@@ -1,10 +1,9 @@
 import * as React from "react"
-import { cn } from "@/lib/utils"
 
-const RadioGroup = React.forwardRef(({ className, ...props }, ref) => {
+const RadioGroup = React.forwardRef(({ className = "", ...props }, ref) => {
   return (
     <div
-      className={cn("grid gap-2", className)}
+      className={`grid gap-2 ${className}`}
       ref={ref}
       {...props}
     />
@@ -12,14 +11,11 @@ const RadioGroup = React.forwardRef(({ className, ...props }, ref) => {
 })
 RadioGroup.displayName = "RadioGroup"
 
-const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
+const RadioGroupItem = React.forwardRef(({ className = "", ...props }, ref) => {
   return (
     <input
       type="radio"
-      className={cn(
-        "h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-        className
-      )}
+      className={`h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       ref={ref}
       {...props}
     />
